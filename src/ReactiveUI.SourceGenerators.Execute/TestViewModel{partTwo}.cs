db37535d4f81f680e053bf1e30ed64fa7343987d
Chris Pulman <chris.pulman@yahoo.com>
2024-08-07 00:02:43 +0100

    Fix for #24
    Change from class analyser to method analyser

---
+++ src/ReactiveUI.SourceGenerators.Execute/TestViewModel{partTwo}.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2024 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI.SourceGenerators;
+
+namespace SGReactiveUI.SourceGenerators.Test
+{
+    /// <summary>
+    /// TestViewModel.
+    /// </summary>
+    /// <seealso cref="ReactiveUI.ReactiveObject" />
+    public partial class TestViewModel
+    {
+        /// <summary>
+        /// Test2s this instance.
+        /// </summary>
+        /// <returns>Rectangle.</returns>
+        [ReactiveCommand]
+        private Point Test2() => default;
+    }
+}
