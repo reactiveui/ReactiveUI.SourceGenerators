27e9ab2caca4a9e199ae30ae934f14613d9e934b
Chris Pulman <chris.pulman@yahoo.com>
2024-08-31 14:41:51 +0100

    Feature Add ViewModelControlHost for WinForms (#37)
    * Feature Add ViewModelControlHost for WinForms
    
    * correct variable name

---
+++ src/ReactiveUI.SourceGenerators.Execute/TestWinFormsVMCHost.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2024 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI.SourceGenerators.WinForms;
+
+namespace SGReactiveUI.SourceGenerators.Test
+{
+    /// <summary>
+    /// TestWinFormsVMCHost.
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.UserControl" />
+    /// <seealso cref="ReactiveUI.IReactiveObject" />
+    /// <seealso cref="ReactiveUI.IViewFor" />
+    [ViewModelControlHost(nameof(UserControl))]
+    public partial class TestWinFormsVMCHost;
+}
