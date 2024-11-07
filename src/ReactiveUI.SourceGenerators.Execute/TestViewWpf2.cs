ace3c5834a92383306a8798b6421423f6303cbc7
Chris Pulman <chris.pulman@yahoo.com>
2024-11-07 00:17:25 +0000

    Fix For Generators used on Generic class

---
+++ src/ReactiveUI.SourceGenerators.Execute/TestViewWpf2.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2024 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Windows;
+using ReactiveUI.SourceGenerators;
+
+namespace SGReactiveUI.SourceGenerators.Test;
+
+/// <summary>
+/// TestViewWpf2.
+/// </summary>
+/// <seealso cref="System.Windows.Window" />
+[IViewFor("TestViewModel2<int>")]
+public partial class TestViewWpf2 : Window
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestViewWpf2"/> class.
+    /// </summary>
+    public TestViewWpf2() => ViewModel = new();
+}
