37600596eb16381b24d8d54d4668480a2fb86395
Chris Pulman <chris.pulman@yahoo.com>
2025-07-19 01:35:21 +0100

    merge

---
+++ src/ReactiveUI.SourceGenerators.Roslyn/ReactiveCollection/ReactiveCollectionGenerator.Execute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactiveUI.SourceGenerators.Roslyn.ReactiveCollection
+{
+    class ReactiveCollectionGenerator
+    {
+    }
+}
