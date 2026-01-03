fdc8310f765a84d7f19f62780b1829da38228db9
Chris Pulman <chris.pulman@yahoo.com>
2026-01-03 11:41:49 +0000

    Housekeeping Update copyright year to 2026
    Updated copyright notices from 2025 to 2026 across source files, test files, and generated code to reflect the new year. No functional code changes were made.

--- src/ReactiveUI.SourceGenerators.Analyzers.CodeFixes/Core/Extensions/FieldSyntaxExtensions.cs
+++ src/ReactiveUI.SourceGenerators.Analyzers.CodeFixes/Core/Extensions/FieldSyntaxExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2025 ReactiveUI and contributors. All rights reserved.
+﻿// Copyright (c) 2026 ReactiveUI and contributors. All rights reserved.
 // Licensed to the ReactiveUI and contributors under one or more agreements.
 // The ReactiveUI and contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
